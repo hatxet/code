c2c23792ebac999c244354db14446b67a1a339e2
Hatchet <hatchet.codes@gmail.com>
2018-01-14 23:25:39 -0600

    Decode the Morse code Part 2

--- src/katas/MorseCodeDecoder.java
+++ src/katas/MorseCodeDecoder.java
@@ -4,12 +4,30 @@ import java.util.HashMap;
 import java.util.Map;
 
 /*
+ * Part 1/3
  * Decode the Morse code
  * The Morse code table is preloaded for you as a dictionary.
  * 
  * https://www.codewars.com/kata/54b724efac3d5402db00065e/
+ * 
+ * Part 2/3 
+ * For this kata we assume the message receiving is performed automatically by the hardware that checks the line periodically.
+ * If the line is connected 1 is recorded. If the line is not connected 0 is recorded.
+ * After the message is fully received, it gets to you for decoding as a string containing only symbols 0 and 1.
+ * If you have trouble discerning if the particular sequence of 1's is a dot or a dash, assume it's a dot.
+ * For example, the message HEY JUDE, that is ···· · −·−−   ·−−− ··− −·· · may be received as follows:
+ * 1100110011001100000011000000111111001100111111001111110000000000000011001111110011111100111111000000110011001111110000001111110011001100000011
+ * 
+ * http://www.codewars.com/kata/decode-the-morse-code-advanced
  */
 public class MorseCodeDecoder {
+	public static final String WORD_DELIMITER = "   ";
+	public static final char LETTER_DELIMITER = ' ';
+	public static final char DOT = '.';
+	public static final char DASH = '-';
+	public static final char ONE = '1';
+	public static final char ZERO = '0';
+	public static final char INVALID = 'X';
 	public static final Map<String, String> MorseCode;
 	static {
 		MorseCode = new HashMap<>();
@@ -71,20 +89,151 @@ public class MorseCodeDecoder {
 	}
 
 	/*
+	 * Converts a string of bits to Morse code
+	 */
+	private static class BitsConverter {
+		public int pulseLength, repeatedPulses, currentBitIndex;
+		public char lastPulse, currentPulse;
+		String bits;
+		StringBuilder morse;
+
+		public BitsConverter(String bits) {
+			this.repeatedPulses = 1;
+			this.currentBitIndex = 0;
+			this.bits = bits;
+			this.morse = new StringBuilder();
+			convert();
+		}
+
+		private void convert() {
+			if (bits.isEmpty()) return;
+
+			// Skip leading 0s
+			while (currentBitIndex < bits.length() && bits.charAt(currentBitIndex) == '0') {
+				++currentBitIndex;
+			}
+			// Only 0s
+			if (currentBitIndex == bits.length()) return;
+
+			calculatePulseLength();
+
+			// Only 1s
+			if (pulseLength == bits.length()) {
+				morse.append(DOT);
+				return;
+			}
+
+			// Start scanning from first positive pulse
+			lastPulse = ZERO;
+			currentPulse = ONE;
+			scanPulses();
+		}
+
+		// A pulse is the minimum time unit that can be considered a dot.
+		// Find the smallest substring of 1s between 0s.
+		private void calculatePulseLength() {
+			char prev, curr = ONE;
+			int repeatedOnes = 0;
+			// Max length is the length of the whole string
+			pulseLength = bits.length();
+			// Assuming we start from a 1 since we skipped all 0s
+			// Stop if length is 1 or it's the end of the string
+			for (int index = currentBitIndex; index < bits.length() && pulseLength > 1; ++index) {
+				prev = curr;
+				curr = bits.charAt(index);
+				if (prev == curr) {
+					++repeatedOnes;
+				} else {
+					if (repeatedOnes < pulseLength) {
+						pulseLength = repeatedOnes;
+					}
+					repeatedOnes = 1;
+				}
+			}
+		}
+
+		private void scanPulses() {
+			while (isValidPulse()) {
+				parsePulse();
+				calculateNextPulse();
+			}
+			if (lastPulse == ONE && repeatedPulses == 1) morse.append(DOT);
+		}
+
+		private void calculateNextPulse() {
+			currentBitIndex += pulseLength;
+			lastPulse = currentPulse;
+			try {
+				currentPulse = bits.charAt(currentBitIndex);
+			} catch (StringIndexOutOfBoundsException e) {
+				currentPulse = INVALID;
+			}
+
+		}
+
+		/*
+		 * 1x ONE pulse = dot
+		 * 3x ONE pulses = dash
+		 * 1x ZERO pulse = dots/dashes delimiter
+		 * 3x ZERO pulses = letter delimiter
+		 * 7x ZERO pulses = word delimiter
+		 */
+		private void parsePulse() {
+			if (currentPulse == lastPulse) {
+				++repeatedPulses;
+				if (currentPulse == ZERO) {
+					if (repeatedPulses % 7 == 0) {
+						morse.append(WORD_DELIMITER);
+					}
+				} else {
+					if (repeatedPulses % 3 == 0) {
+						morse.append(DASH);
+					}
+				}
+			} else {
+				// Previous pulse is different
+				if (currentPulse == ZERO) {
+					if (repeatedPulses == 1) {
+						morse.append(DOT);
+					}
+				} else if (repeatedPulses % 3 == 0) {
+					morse.append(LETTER_DELIMITER);
+				}
+				repeatedPulses = 1;
+			}
+		}
+
+		private boolean isValidPulse() {
+			// return currentPulse != INVALID
+			return currentPulse == ZERO || currentPulse == ONE;
+		}
+
+		@Override
+		public String toString() {
+			return morse.toString();
+		}
+
+	}
+
+	public static String decodeBits(String bits) {
+		return new BitsConverter(bits).toString();
+	}
+
+	/*
 	 * Third try: Iterating over string with less checks.
 	 * It can't be faster than my second try but it's more readable.
 	 * Server timed out once again. I give up.
 	 */
-	public static String decode(String morseCode) {
+	public static String decodeMorse(String morseCode) {
 		String cleanMorseCode = morseCode.trim();
 		StringBuffer message = new StringBuffer();
-		String[] words = cleanMorseCode.split("   ");
+		String[] words = cleanMorseCode.split(WORD_DELIMITER);
 		for (String word : words) {
-			String[] letters = word.split(" ");
+			String[] letters = word.split(Character.toString(LETTER_DELIMITER));
 			for (String letter : letters) {
 				message.append(MorseCode.get(letter));
 			}
-			message.append(' ');
+			message.append(LETTER_DELIMITER);
 		}
 		message.setLength(message.length() - 1);
 		return message.toString();
@@ -101,7 +250,7 @@ public class MorseCodeDecoder {
 	//
 	//		for (int i = 0; i < morseCode.length(); ++i) {
 	//			char c = morseCode.charAt(i);
-	//			if (c != ' ') {
+	//			if (c != DOT) {
 	//				letter.append(c);
 	//				countSpaces = 0;
 	//			} else {
@@ -110,7 +259,7 @@ public class MorseCodeDecoder {
 	//					message.append(MorseCode.get(letter.toString()));
 	//					letter = new StringBuffer();
 	//				} else if (countSpaces == 2) {
-	//					message.append(' ');
+	//					message.append(DOT);
 	//				}
 	//			}
 	//		}
@@ -142,7 +291,7 @@ public class MorseCodeDecoder {
 	//			for (String letter : letters) {
 	//				message.append(MorseCode.get(letter));
 	//			}
-	//			message.append(' ');
+	//			message.append(DOT);
 	//		}
 	//		// Remove last space
 	//		message.setLength(message.length() - 1);
--- test/katas/MorseCodeDecoderTest.java
+++ test/katas/MorseCodeDecoderTest.java
@@ -1,5 +1,6 @@
 package katas;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -7,23 +8,51 @@ import org.junit.jupiter.api.Test;
 class MorseCodeDecoderTest {
 
 	@Test
-	public void testSimple() {
-		assertTrue(MorseCodeDecoder.decode(".-").equals("A"));
-		assertTrue(MorseCodeDecoder.decode(".").equals("E"));
-		assertTrue(MorseCodeDecoder.decode("..").equals("I"));
-		assertTrue(MorseCodeDecoder.decode(". .").equals("EE"));
-		assertTrue(MorseCodeDecoder.decode(".   .").equals("E E"));
-		assertTrue(MorseCodeDecoder.decode("...---...").equals("SOS"));
-		assertTrue(MorseCodeDecoder.decode("... --- ...").equals("SOS"));
+	public void decodeMorseSimple() {
+		assertTrue(MorseCodeDecoder.decodeMorse(".-").equals("A"));
+		assertTrue(MorseCodeDecoder.decodeMorse(".").equals("E"));
+		assertTrue(MorseCodeDecoder.decodeMorse("..").equals("I"));
+		assertTrue(MorseCodeDecoder.decodeMorse(". .").equals("EE"));
+		assertTrue(MorseCodeDecoder.decodeMorse(".   .").equals("E E"));
+		assertTrue(MorseCodeDecoder.decodeMorse("...---...").equals("SOS"));
+		assertTrue(MorseCodeDecoder.decodeMorse("... --- ...").equals("SOS"));
 	}
 
 	@Test
-	public void testMore() {
-		assertTrue(MorseCodeDecoder.decode(" . ").equals("E"));
-		assertTrue(MorseCodeDecoder.decode("   .   . ").equals("E E"));
-		assertTrue(MorseCodeDecoder.decode(
+	public void decodeMorseExtra() {
+		assertTrue(MorseCodeDecoder.decodeMorse(".   .").equals("E E"));
+		assertTrue(MorseCodeDecoder.decodeMorse(" . ").equals("E"));
+		assertTrue(MorseCodeDecoder.decodeMorse("   .   . ").equals("E E"));
+		assertTrue(MorseCodeDecoder.decodeMorse(
 				"      ...---... -.-.--   - .... .   --.- ..- .. -.-. -.-   -... .-. --- .-- -.   ..-. --- -..-   .--- ..- -- .--. ...   --- ...- . .-.   - .... .   .-.. .- --.. -.--   -.. --- --. .-.-.-  ")
 				.equals("SOS! THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG."));
+	}
+
+	@Test
+	public void decodeBitsSimple() throws Exception {
+		String[] expectedBin = new String[] { "1110111", "000000000000", "011111111110", "111111",
+				"0000111000111111111000000000111000000", "000011000000111111",
+				"00001110000000000000000000001110000000000000000000000" };
+		String[] expectedMorse = new String[] { "--", "", ".", ".", ".- .", ". -", ".   .   " };
+
+		for (int i = 0; i < expectedBin.length; ++i) {
+			assertTrue("(" + i + ")=> " + expectedMorse[i],
+					MorseCodeDecoder.decodeBits(expectedBin[i]).equals(expectedMorse[i]));
+		}
+	}
+
+	@Test
+	public void decodeBitsHeyJude() throws Exception {
+		assertTrue(MorseCodeDecoder.decodeMorse(MorseCodeDecoder.decodeBits(
+				"00001100110011001100000011000000111111001100111111001111110000000000000011001111110011111100111111000000110011001111110000001111110011001100000011"))
+				.equals("HEY JUDE"));
+	}
+
+	@Test
+	public void decodeLongMessage() {
+		assertEquals("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.",
+				MorseCodeDecoder.decodeMorse(MorseCodeDecoder.decodeBits(
+						"00011100010101010001000000011101110101110001010111000101000111010111010001110101110000000111010101000101110100011101110111000101110111000111010000000101011101000111011101110001110101011100000001011101110111000101011100011101110001011101110100010101000000011101110111000101010111000100010111010000000111000101010100010000000101110101000101110001110111010100011101011101110000000111010100011101110111000111011101000101110101110101110")));
 	}
 
 }
